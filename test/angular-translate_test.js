3dff44e277682a51cb2781202676e582eac4d708
Benjamin Longearet <blongearet@teads.tv>
2013-11-15 02:36:15 +0100

    Update tests

--- test/angular-translate_test.js
+++ test/angular-translate_test.js
@@ -8,28 +8,64 @@ exports.i18nextract = {
     test.expect(1);
 
     var actual = grunt.file.read('tmp/00_fr_FR.json');
+    grunt.log.writeflags(actual);
     var expected = grunt.file.read('test/expected/00_fr_FR.json');
-    test.equal(actual, expected, 'should describe what the default behavior is.');
+    grunt.log.writeflags(expected);
+    test.equal(actual, expected, '00 - default_options should be equal.');
 
     test.done();
   },
 
-  exists_i18n: function(test) {
+  default_exists_i18n: function(test) {
     test.expect(1);
 
     var actual = grunt.file.read( 'tmp/01_fr_FR.json' );
     var expected = grunt.file.read( 'test/expected/01_fr_FR.json' );
-    test.equal( actual, expected, 'should describe what the default behavior is when lang file already exist.' );
+    test.equal( actual, expected, '01 - default_exists_i18n should be equal.' );
 
     test.done();
   },
 
-  deleted_i18n: function(test) {
+  default_deleted_i18n: function(test) {
     test.expect(1);
 
     var actual = grunt.file.read( 'tmp/02_fr_FR.json' );
     var expected = grunt.file.read( 'test/expected/02_fr_FR.json' );
-    test.equal( actual, expected, 'should describe what the default behavior is when lang file already exist.' );
+    test.equal(actual, expected, '02 - default_deleted_i18n should be equal.');
+
+    test.done();
+  },
+
+  interpolation_bracket: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read( 'tmp/03_fr_FR.json' );
+    var expected = grunt.file.read( 'test/expected/03_fr_FR.json' );
+    test.equal(actual, expected, '03 - interpolation_bracket should be equal.');
+
+    test.done();
+  },
+
+  default_language: function(test) {
+    test.expect(2);
+
+    var actual = grunt.file.read( 'tmp/04_fr_FR.json' );
+    var expected = grunt.file.read( 'test/expected/04_fr_FR.json' );
+    test.equal( actual, expected, '04 - default_language fr_FR should be equal.' );
+
+    var actual = grunt.file.read( 'tmp/04_en_US.json' );
+    var expected = grunt.file.read( 'test/expected/04_en_US.json' );
+    test.equal( actual, expected, '04 - default_language en_US should be equal.' );
+
+    test.done();
+  },
+
+  json_extract: function(test) {
+    test.expect(1);
+
+    var actual = grunt.file.read( 'tmp/05_en_US.json' );
+    var expected = grunt.file.read( 'test/expected/05_en_US.json' );
+    test.equal( actual, expected, 'Should equal.' );
 
     test.done();
   }
